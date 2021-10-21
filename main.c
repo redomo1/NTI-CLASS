727484b69189ba8d94c974c10ecae92220c311d7
redomo1 <rehabsoliman98@gmail.com>
2021-10-21 11:45:34 +0200

    test in new test pranch

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 
 int main()
 {
-    int x=20;
-    int*p=NULL;  //NULL pointer
-    p=&x;
-    *p=50;
-    printf("%d",x);
+    int arr[3][3]={{10,20,30},{40,50,60},{70,80,90}}; //rows coulmn
+    int arr[3][3]={10,20,30,40,50,60,70,80,90};
+
+    int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+   }
     return 0;
 }
